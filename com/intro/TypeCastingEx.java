43d52cb35fb84cf6e2a84bd49ebdd684e27c3eeb
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2024-02-25 20:31:42 +0530

    Add files via upload

---
+++ com/intro/TypeCastingEx.java
@@ -0,0 +1,31 @@
+
+public class TypeCastingEx {
+	
+	public static void main(String[] args) {
+	
+		
+		//Implicit typecasting ---->It's automatic
+				int i=10;			//4
+				
+				long l = i;		//8
+		
+				System.out.println(l);
+				
+				
+			//Explicit TypeCasting------>
+				
+				float a = 3.14f;
+				
+				int b = (int)a;
+				
+				long c= 398749348;
+				
+				byte d = (byte)c;		//-128->127
+				
+				System.out.println(d);
+		
+				
+		
+	}
+	
+}
