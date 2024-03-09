cdedca86e73b573a5fd99b1938a2a8189366e4c0
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2024-03-09 20:25:06 +0530

    Add files via upload

--- com/casestudy/Client.java
+++ com/casestudy/Client.java
@@ -1,7 +1,11 @@
 package com.casestudy;
 
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Client {
 
@@ -10,6 +14,8 @@ public class Client {
 		Scanner scan = new Scanner(System.in);
 
 		HashMap<Long, Account> accounts = new HashMap<Long, Account>();
+		
+		HashMap<Integer,Transaction> transactions = new HashMap<Integer,Transaction>();
 
 		long accNo;
 
@@ -77,23 +83,163 @@ public class Client {
 			}
 			case 2: {
 				System.out.println("********CHECK BALANCE********");
+				
+				System.out.print("Enter your account number:-");
+				
+				accNo =scan.nextLong();
+				
+				account = accounts.get(accNo);
+				
+				float Balance = account.getAccBal();
+				
+				System.out.println("Your account balance is:-"+Balance);
+				
 				break;
 			}
 			case 3: {
 
 				System.out.println("********DEPOSIT********");
+				
+				System.out.print("Enter your account number:-");
+				
+				accNo = scan.nextLong();
+				
+				System.out.print("Enter your deposit amount:-");
+				
+				float depositAmount = scan.nextFloat();
+				
+				account = accounts.get(accNo);
+				
+				float existingAmount = account.getAccBal();
+				
+				float finalAmount = existingAmount + depositAmount;
+				
+				account.setAccBal(finalAmount);
+				
+				System.out.println("Your amount is deposited successfully ");
+				
+				System.out.println("Your current balance is:-"+finalAmount);
+				
+				LocalDate date = LocalDate.now();
+				
+				Random  random = new Random();
+				
+				int transId = random.nextInt();
+				
+				Transaction transaction = new Transaction(accNo, "deposit", date, 0, depositAmount, finalAmount, transId);
+				
+				transactions.put(transId, transaction);
+				
 				break;
 			}
 			case 4: {
 				System.out.println("********WITHDRAW********");
+				
+				System.out.println("Enter your account number:-");
+				
+				accNo = scan.nextLong();
+				
+				System.out.println("Enter your withdrawl amount:-");
+				
+				float withdrawlAmount = scan.nextFloat();
+				
+				account = accounts.get(accNo);
+				
+				float existingAmount = account.getAccBal();
+				
+				float finalAmount = existingAmount - withdrawlAmount ;
+				
+				account.setAccBal(finalAmount);
+				
+				System.out.println("your amount is debited successfully");
+				
+				System.out.println("The current balance is:-"+finalAmount);
+				
+				LocalDate date = 	LocalDate.now();
+				
+				Random random = new Random();
+				
+				int transId= random.nextInt();
+				
+				Transaction transaction = new Transaction(accNo, "withdraw", date, withdrawlAmount, 0, finalAmount, transId);
+				
+				transactions.put(transId,transaction);
+				
 				break;
 			}
 			case 5: {
 				System.out.println("********FUND TRANSFER********");
+				
+				System.out.println("Enter your account number:-");
+				
+				long accNoFrom = scan.nextLong();
+				
+				System.out.println("Enter your to account number:-");
+				
+				long accNoTo = scan.nextLong();
+				
+				System.out.println("Enter your amount to transfer:-");
+				
+				float transferAmount = scan.nextFloat();
+				
+				Account fromAccount = accounts.get( accNoFrom);
+				
+				Account toAccount = accounts.get(accNoTo);
+				
+				float fromAccountBalance = fromAccount.getAccBal();
+				
+				float toAccountBalance = toAccount.getAccBal();
+				
+				fromAccountBalance = fromAccountBalance - transferAmount ;
+				
+				toAccountBalance = toAccountBalance + transferAmount;
+				
+				fromAccount.setAccBal(fromAccountBalance);
+				
+				toAccount.setAccBal(toAccountBalance);
+				
+				System.out.println("Amount is transferred successfully");
+				
+				System.out.println("The current balance:-"+fromAccountBalance);
+				
+				LocalDate date = LocalDate.now();
+				
+				Random random = new Random();
+				
+				int transId = random.nextInt();
+				
+				Transaction transaction = new Transaction(accNoFrom,"fundTransfer", date, transferAmount, 0, fromAccountBalance, transId);
+				
+				transactions.put(transId,transaction);
+				
 				break;
 			}
 			case 6: {
 				System.out.println("********BANK STATEMENT********");
+				
+				System.out.printf("%-15s%-15s%-15s%-15s%-15s","accNo", "typeOfTrans", "Date", "withdrawlAmount", "depositAmount",
+						"balance"," transId");
+				
+				System.out.println();
+				
+				Set<Integer> set = transactions.keySet();
+				
+				Iterator itr = set.iterator();
+				
+				while(itr.hasNext()) {
+					int key = (int) itr.next();
+					
+					Transaction trans = transactions.get(key);
+					
+					System.out.println(trans.getAccNo()+"   "+trans.getTypeOfTrans()+"   "+trans.getDate()+"   "+trans.getWithdrawlAmount()+"   "+trans.getDepositAmount()+"   "+
+					trans.getBalance()+"   "+trans.getTransId());
+					
+					
+					
+				}
+				
+				value = false;
+				
 				break;
 			}
 			
